9b37819d410bd3428f5f568301d84e574d1c755e
Kat Reinhart <kat.reinhart@gmail.com>
2017-08-27 11:08:30 -0700

    Add main and signin components

---
+++ src/Main.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react';
+import {
+  Navigator
+} from 'react-native';
+
+import SignIn from './src/components/auth/signin';
+
+const routes = {
+  signIn: SignIn
+}
+
+export default class Main extends Component {
+  render() {
+    return(
+      <Navigator
+        initialRoute={{name: 'signIn'}}
+        renderScene={this.renderScene}
+      />
+    )
+  }
+
+  renderScene(route, navigator) {
+    let Component = routes[route.name];
+
+    return(
+      <Component
+        navigator={navigator}
+      />
+
+    )
+  }
+}
